2055917dc856f0f1d1cdcc64acd5c51e38879ffa
Daniel <dgonzalez@jumpitt.com>
2020-10-02 00:53:58 -0300

    ✨ 👽 add login view, routes and more

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="m-5">
+    <b-row align-h="center">
+      <b-col sm="12" xl="6">
+        <b-embed
+          type="iframe"
+          aspect="16by9"
+          src="https://www.youtube.com/embed/woUZdM7qNjk?rel=0"
+          allowfullscreen
+        ></b-embed>
+        <div my-4>
+          <h4>Rating</h4>
+          <b-form-rating v-model="value"></b-form-rating>
+          <p class="mt-2">Value: {{ value }}</p>
+        </div>
+      </b-col>
+    </b-row>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: null,
+    };
+  },
+};
+</script>
