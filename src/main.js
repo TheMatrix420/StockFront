d881b34e04e62875e5759ea571fd548acbffbbcb
Jota-Wolf <lobos.joaquin@hotmail.com>
2020-10-03 20:20:47 -0300

    :bug: Login con bug

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+import "@babel/polyfill";
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(axios);
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.config.productionTip = false;
+axios.defaults.baseURL = "http://localhost:4000/api/";
 
-Vue.config.productionTip = false
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,62 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home'
-import About from '../views/About'
-import Login from '../views/Auth/Login'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home";
+import About from "../views/About";
+import Login from "../views/Auth/Login";
+import store from "../store";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
+    path: "/home",
+    name: "Home",
+    component: Home,
+    meta: {
+      title: "Home",
+      user: true,
+    },
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/about",
+    name: "About",
+    component: About,
+    meta: {
+      title: "About",
+      administrador: true,
+    },
   },
   {
-    path: '/',
-    name: 'Login',
-    component: Login
-  }
-]
+    path: "/",
+    name: "Login",
+    component: Login,
+    meta: {
+      title: "Login",
+      free: true,
+    },
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.free)) {
+    next();
+  } else if (store.state.usuario && store.state.usuario.role == "user_role") {
+    if (to.matched.some((record) => record.meta.user)) {
+      next();
+    }
+  } else if (store.state.usuario && store.state.usuario.role == "admin_role") {
+    if (to.matched.some((record) => record.meta.administrador)) {
+      next();
+    }
+  } else {
+    next({ name: "Login" });
+  }
+});
+
+export default router;
